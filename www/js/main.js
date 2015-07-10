5d57c12b5712b9a0c146b993775eee4f7f1fdb03
coreytegeler <coreytegeler@gmail.com>
2015-07-10 13:53:16 -0400

    bundled into cordova ios app

---
+++ www/js/main.js
@@ -0,0 +1,40 @@
+$(function() {
+
+	$('section').each(function(i, section) {
+		$(section).css({
+			y : $(section).innerHeight()*i,
+		}).attr('data-count',i);
+
+		$('canvas#canvas').css({
+			height: h() - 90,
+			width: w()
+		});
+	});
+
+	$('section header').click(function() {
+		$section = $(this).parent('section');
+		if(!$section.hasClass('opened')) {
+			$section.addClass('opened').addClass('intro');
+			var distY = $section.offset().top;
+			var count = $section.attr('data-count');
+			var speed = 200 + count/50;
+			$section.transition({y:0, height:h()}, speed);
+			$('body').animate({ scrollTop: "0" }, speed);
+			$('body').removeClass('home').addClass('intro');
+		}
+	});
+
+	$('section .begin').click(function() {
+		var cue = $(this).attr('data-cue');
+		beginCue(cue);
+	});
+
+	function beginCue(cue) {
+		$section = $('section#'+cue);
+		$('body').removeClass('intro').addClass('onCue');
+		$section.removeClass('intro').addClass('dark');
+		$('#easel').transition({opacity:1, delay: 00}, 130);
+		createCanvas();
+	}
+
+});
